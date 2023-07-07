c4e54e53ab4b525b3baaf96dfdedc1c73f1ae444
Marceeeeeee <126773680+Marceeeeeee@users.noreply.github.com>
2023-07-07 12:10:32 +0200

    armi e sparo

--- Esame Davide Marcellino/Assets/Script/EnemyBehaviour.cs
+++ Esame Davide Marcellino/Assets/Script/EnemyBehaviour.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float health = 50f;
+
+    public void TakeDamage (float amount) {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void GameOver (float amount) {
+        health -= amount;
+        if (health <= 0f)
+        {
+            GameOverCiao();
+        }
         
     }
+
+    void Die(){
+        Destroy(gameObject);
+    }
+
+    void GameOverCiao(){
+    SceneManager.LoadScene(2);
+    }
+
 }
---
+++ Esame Davide Marcellino/Assets/Script/Gun.cs
@@ -0,0 +1,35 @@
+
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 50f;
+    public float range = 100f;
+
+    public Camera fpsCamera;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+            EnemyBehaviour target = hit.transform.GetComponent<EnemyBehaviour>();
+            TankBehaviour tank = hit.transform.GetComponent<TankBehaviour>();
+            if(target != null){
+                if(hit.transform.name == "Cylinder"){
+                tank.TakeDamage(damage);
+                }else
+                {
+                target.TakeDamage(damage);
+                }
+            }
+        }
+
+    }
+}
